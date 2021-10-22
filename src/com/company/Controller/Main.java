6697033b9c217b90c4060adfc0d4b17e196da7fa
ramprashant66 <ramprashant66@gmail.com>
2021-10-22 14:39:25 -0700

    MVC 2.0

--- src/com/company/Controller/Main.java
+++ src/com/company/Controller/Main.java
@@ -2,6 +2,7 @@ package com.company.Controller;
 
 import com.company.Model.CreateUser;
 import com.company.View.Display;
+import com.company.View.Menu;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -18,18 +19,14 @@ public class Main {
         //creating a new text file which will hold the user's information
         FileWriter userFile = new FileWriter("src/com/userFile.txt");
 
-        //New scanner object to work with
-        Scanner scan = new Scanner(System.in);
-        String exitProgram = "n";       //initialize exitProgram to "n".
+        String exitProgram = "N";       //initialize exitProgram to "N".
 
         //Do these while the exitProgram is "n"
-        while (exitProgram.equals("n"))
+        while (exitProgram.equals("N"))
         {
+            Menu.menu(); //shows user options
 
-            System.out.println("1. new user\n2.Update\nEnter option");      //show options
-            int optionChosen = scan.nextInt();      //gets the option chosen from user.
-
-            if (optionChosen == 1)
+            if (Menu.optionChosen() == 1)
             {
                 CreateUser user = new CreateUser();  //creates a new user.
                 userList[count] = user;             //adds the user to the userList array.
@@ -38,13 +35,12 @@ public class Main {
                 user.storeData(userFile);           //write the data to the text file.
             }
 
-            else if (optionChosen == 2)
+            else if (Menu.optionChosen() == 2)
             {
                 Display.update(userList);       //update current user information
             }
 
-            System.out.println("Exit?");        //prompt for exit
-            exitProgram = scan.next();          //get input for exitProgram
+            exitProgram = Menu.endProgram();        //exit program?
         }
 
         userFile.close();                   //close the user file
